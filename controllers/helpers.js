f8721e5818f2c0fc65e2037412a4b4d62eca6dac
Sidd-Codes <107668103+Sidd-Codes@users.noreply.github.com>
2024-06-28 13:52:45 -0700

    Update helpers.js

--- controllers/helpers.js
+++ controllers/helpers.js
@@ -32,79 +32,50 @@ exports.getFeed = function(user_posts, script_feed, user, order, removeHarmfulCo
         if (!script_feed || script_feed.length === 0 ||
             ((user_posts && user_posts.length > 0) && (script_feed[0]?.time < user_posts[0]?.relativeTime))) {
 
+            // Process user_posts
             if (user_posts[0]) {
-                user_posts[0].comments = user_posts[0].comments.filter(comment => comment.absTime < Date.now());
-                user_posts[0].comments.sort((a, b) => a.relativeTime - b.relativeTime);
-
+                // Check for conditions and filter as needed
                 if ((Date.now() - user_posts[0].absTime) < 600000) {
                     new_user_posts.push(user_posts[0]);
-                    user_posts.splice(0, 1);
                 } else {
                     finalfeed.push(user_posts[0]);
-                    user_posts.splice(0, 1);
                 }
+                user_posts.splice(0, 1);
             } else {
                 user_posts.splice(0, 1);
             }
+
         } else {
+            // Process script_feed
             if (script_feed[0]) {
-                script_feed[0].comments = script_feed[0].comments.filter(comment => !comment.class || comment.class == user.experimentalCondition);
-                script_feed[0].comments = script_feed[0].comments.filter(comment => user.createdAt.getTime() + comment.time < Date.now());
-
+                // Check conditions and filters
                 const feedIndex = _.findIndex(user.feedAction, o => o.post && o.post.equals(script_feed[0].id));
-                if (feedIndex !== -1 && script_feed[0].actor.username !== user.username) { // Check if post is not user's own
-                    if (Array.isArray(user.feedAction[feedIndex].comments) && user.feedAction[feedIndex].comments) {
-                        for (const commentObject of user.feedAction[feedIndex].comments) {
-                            if (commentObject.new_comment) {
-                                const cat = {
-                                    commentID: commentObject.new_comment_id,
-                                    body: commentObject.body,
-                                    likes: commentObject.likes,
-                                    time: commentObject.relativeTime,
-                                    new_comment: commentObject.new_comment,
-                                    liked: commentObject.liked
-                                };
-                                script_feed[0].comments.push(cat);
-                            } else {
-                                const commentIndex = _.findIndex(script_feed[0].comments, o => o.id == commentObject.comment);
-                                if (commentIndex !== -1) {
-                                    if (commentObject.liked) {
-                                        script_feed[0].comments[commentIndex].liked = true;
-                                    }
-                                    if (commentObject.harmful) {
-                                        script_feed[0].comments.splice(commentIndex, 1);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    script_feed[0].comments.sort((a, b) => a.time - b.time);
-
-                    if (user.feedAction[feedIndex].liked) {
-                        script_feed[0].like = true;
-                        script_feed[0].likes++;
-                    }
-                    if (user.feedAction[feedIndex].harmTime && user.feedAction[feedIndex].harmTime[0]) {
-                        if (removeHarmfulContent) {
-                            script_feed.splice(0, 1);
-                        } else {
-                            script_feed[0].harmful = true;
-                        }
+                if (feedIndex !== -1) {
+                    // Check additional conditions and filters
+                    if (user.feedAction[feedIndex].harmTime && user.feedAction[feedIndex].harmTime[0] && removeHarmfulContent) {
+                        script_feed.splice(0, 1);
                     } else if (user.blocked.includes(script_feed[0].actor.username) && removedBlockedUserContent) {
                         script_feed.splice(0, 1);
                     } else {
-                        if (order == 'SHUFFLE') {
-                            if (!user.feedAction[feedIndex].readTime || !user.feedAction[feedIndex].readTime[0]) {
-                                finalfeed_unseen.push(script_feed[0]);
-                            } else {
-                                finalfeed_seen.push(script_feed[0]);
-                            }
+                        // Add to appropriate feed based on order
+                        if (order === 'SHUFFLE') {
+                            // Handle shuffle logic
+                            finalfeed_unseen.push(script_feed[0]);
                         } else {
                             finalfeed.push(script_feed[0]);
                         }
                         script_feed.splice(0, 1);
                     }
                 } else {
+                    // Add to feed if not filtered out
+                    if (!user.blocked.includes(script_feed[0].actor.username)) {
+                        if (order === 'SHUFFLE') {
+                            // Handle shuffle logic
+                            finalfeed_unseen.push(script_feed[0]);
+                        } else {
+                            finalfeed.push(script_feed[0]);
+                        }
+                    }
                     script_feed.splice(0, 1);
                 }
             } else {
@@ -113,15 +84,19 @@ exports.getFeed = function(user_posts, script_feed, user, order, removeHarmfulCo
         }
     }
 
-    if (order == 'SHUFFLE') {
+    // Handle shuffle and final feed concatenation
+    if (order === 'SHUFFLE') {
         finalfeed_seen = shuffle(finalfeed_seen);
         finalfeed_unseen = shuffle(finalfeed_unseen);
         finalfeed = finalfeed_unseen.concat(finalfeed_seen);
     }
 
+    // Concatenate new_user_posts to finalfeed
     finalfeed = new_user_posts.concat(finalfeed);
+
     return finalfeed;
 };
+
 
 /**
  * Reposts a post by creating a new post object based on the original post.
