6477d2bf81c053d62a9af7a777a35a0230f8dc8a
Sidd-Codes <107668103+Sidd-Codes@users.noreply.github.com>
2024-07-02 20:22:01 -0700

    Update script.js

--- controllers/script.js
+++ controllers/script.js
@@ -1,140 +1,12 @@
-const Script = require('../models/Script.js');
-const User = require('../models/User');
-const Notification = require('../models/Notification');
-const helpers = require('./helpers');
-const _ = require('lodash');
-const dotenv = require('dotenv');
-dotenv.config({ path: '.env' }); // See the file .env.example for the structure of .env
-
-/**
- * GET /
- * Fetch and render newsfeed.
- */
-exports.getScript = async(req, res, next) => {
-    try {
-        const one_day = 86400000; // Number of milliseconds in a day.
-        const time_now = Date.now(); // Current date.
-        const time_diff = time_now - req.user.createdAt; // Time difference between now and user account creation, in milliseconds.
-        const time_limit = time_diff - one_day; // Date in milliseconds 24 hours ago from now. This is used later to show posts only in the past 24 hours.
-
-        const user = await User.findById(req.user.id)
-            .populate('posts.comments.actor')
-            .exec();
-
-        // If the user is no longer active, sign the user out.
-        if (!user.active) {
-            req.logout((err) => {
-                if (err) console.log('Error : Failed to logout.', err);
-                req.session.destroy((err) => {
-                    if (err) console.log('Error : Failed to destroy the session during logout.', err);
-                    req.user = null;
-                    req.flash('errors', { msg: 'Account is no longer active. Study is over.' });
-                    res.redirect('/login' + (req.query.r_id ? `?r_id=${req.query.r_id}` : ""));
-                });
-            });
-        }
-
-        // What day in the study is the user in? 
-        // Update study_days, which tracks the number of time user views feed.
-        const current_day = Math.floor(time_diff / one_day);
-        if (current_day < process.env.NUM_DAYS) {
-            user.study_days[current_day] += 1;
-            user.save();
-        }
-
-        // Array of actor posts that match the user's experimental condition, within the past 24 hours, sorted by descending time. 
-        let script_feed = await Script.find({
-                class: { "$in": ["", user.experimentalCondition] }
-            })
-            .where('time').lte(time_diff).gte(time_limit)
-            .sort('-time')
-            .populate('actor')
-            .populate('comments.actor')
-            .exec();
-
-        // Array of any user-made posts within the past 24 hours, sorted by time they were created.
-        let user_posts = user.getPostInPeriod(time_limit, time_diff);
-        user_posts.sort(function(a, b) {
-            return b.relativeTime - a.relativeTime;
-        });
-
-        // Get the newsfeed and render it.
-        const finalfeed = helpers.getFeed(user_posts, script_feed, user, process.env.FEED_ORDER, true);
-        console.log("Script Size is now: " + finalfeed.length);
-        res.render('script', { script: finalfeed, showNewPostIcon: true });
-    } catch (err) {
-        next(err);
-    }
-};
-
-/*
- * Post /post/new
- * Record a new user-made post. Include any actor replies (comments) that go along with it.
- */
-exports.newPost = async(req, res, next) => {
-    try {
-        const user = await User.findById(req.user.id).exec();
-        if (req.file) {
-            user.numPosts = user.numPosts + 1; // Count begins at 0
-            const currDate = Date.now();
-
-            let post = {
-                type: "user_post",
-                postID: user.numPosts,
-                body: req.body.body,
-                picture: req.file.filename,
-                liked: false,
-                likes: 0,
-                comments: [],
-                absTime: currDate,
-                relativeTime: currDate - user.createdAt,
-            };
-
-            // Find any Actor replies (comments) that go along with this post
-            const actor_replies = await Notification.find()
-                .where('userPostID').equals(post.postID)
-                .where('notificationType').equals('reply')
-                .populate('actor').exec();
-
-            // If there are Actor replies (comments) that go along with this post, add them to the user's post.
-            if (actor_replies.length > 0) {
-                for (const reply of actor_replies) {
-                    user.numActorReplies = user.numActorReplies + 1; // Count begins at 0
-                    const tmp_actor_reply = {
-                        actor: reply.actor._id,
-                        body: reply.replyBody,
-                        commentID: user.numActorReplies,
-                        relativeTime: post.relativeTime + reply.time,
-                        absTime: new Date(user.createdAt.getTime() + post.relativeTime + reply.time),
-                        new_comment: false,
-                        liked: false,
-                        harmful: false, // Updated: Use harmful instead of flagged
-                        likes: 0
-                    };
-                    post.comments.push(tmp_actor_reply);
-                }
-            }
-            user.posts.unshift(post); // Add most recent user-made post to the beginning of the array
-            await user.save();
-            res.redirect('/');
-        } else {
-            req.flash('errors', { msg: 'ERROR: Your post did not get sent. Please include a photo and a caption.' });
-            res.redirect('/');
-        }
-    } catch (err) {
-        next(err);
-    }
-};
-
 /**
  * POST /feed/
  * Record user's actions on ACTOR posts. 
  */
-exports.postUpdateFeedAction = async(req, res, next) => {
+exports.postUpdateFeedAction = async (req, res, next) => {
     try {
         console.log('postUpdateFeedAction request:', req.body);
         const user = await User.findById(req.user.id).exec();
-        let feedIndex = _.findIndex(user.feedAction, function(o) { return o.post == req.body.postID; });
+        let feedIndex = _.findIndex(user.feedAction, function (o) { return o.post == req.body.postID; });
 
         if (feedIndex == -1) {
             const cat = {
@@ -154,6 +26,10 @@ exports.postUpdateFeedAction = async(req, res, next) => {
             user.feedAction[feedIndex].unlikeTime.push(unlike);
             user.feedAction[feedIndex].liked = false;
             user.numPostLikes--;
+        } else if (req.body.harmful) {
+            const harmful = req.body.harmful;
+            user.feedAction[feedIndex].harmfulTime.push(harmful);
+            user.feedAction[feedIndex].harmful = true;
         }
 
         await user.save();
@@ -168,11 +44,11 @@ exports.postUpdateFeedAction = async(req, res, next) => {
  * POST /userPost_feed/
  * Record user's actions on USER posts, including reposting.
  */
-exports.postUpdateUserPostFeedAction = async(req, res, next) => {
+exports.postUpdateUserPostFeedAction = async (req, res, next) => {
     try {
         console.log('postUpdateUserPostFeedAction request:', req.body);
         const user = await User.findById(req.user.id).exec();
-        let feedIndex = _.findIndex(user.posts, function(o) { return o.postID == req.body.postID; });
+        let feedIndex = _.findIndex(user.posts, function (o) { return o.postID == req.body.postID; });
 
         if (feedIndex == -1) {
             res.send({ result: "error", message: "Post not found" });
@@ -183,6 +59,8 @@ exports.postUpdateUserPostFeedAction = async(req, res, next) => {
             user.posts[feedIndex].liked = true;
         } else if (req.body.unlike) {
             user.posts[feedIndex].liked = false;
+        } else if (req.body.harmful) {
+            user.posts[feedIndex].harmful = true;
         }
 
         await user.save();
